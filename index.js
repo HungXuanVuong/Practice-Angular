858c82bfc5e6b0b91dcd68af9d88ba5c96207cbf
HungVX <masazukass@gmail.com>
2020-07-20 23:45:45 +0700

    test

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 
 function main (){
 	
-	Console.log('Hello World');
+	console.log('Hello World');
+	
+}
+
+function changeValue(value){
+	return value + 1000;
 }
 
 main();
+
+let a = 5;
+
+console.log('value of a after change:' + changeValue(a));
+
+console.log('hello world');
